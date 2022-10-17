268bf6d8b40701c893d1dc89c9f25427874a2f6b
sinnom <106713618+sinnom@users.noreply.github.com>
2022-10-16 20:05:31 -0500

    Renamed press.ts from press.tsx

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,8 @@
 function updateCounter() {
   const pressUrl = "/.netlify/functions/press/";
   fetch(pressUrl).then(
+    // This consistently logs nothing for the response.
+    // Does FaunaDB return any response after a successful Update operation?
     (res) => console.log(`'Press' promise fulfilled: \n${JSON.stringify(res)}`),
     (err) => console.log(`'Press' promise rejected: \n${JSON.stringify(err)}`)
   );
