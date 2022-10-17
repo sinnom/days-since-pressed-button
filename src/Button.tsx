2db77717d465b58159baf9cf8906e4cd12a6f69d
sinnom <106713618+sinnom@users.noreply.github.com>
2022-10-16 19:39:15 -0500

    Remove redundant hostname

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,5 @@
 function updateCounter() {
-  const pressUrl = window.location.hostname + "/.netlify/functions/press/";
+  const pressUrl = "/.netlify/functions/press/";
   fetch(pressUrl).then(
     (res) => console.log(`'Press' promise fulfilled: \n${JSON.stringify(res)}`),
     (err) => console.log(`'Press' promise rejected: \n${JSON.stringify(err)}`)
